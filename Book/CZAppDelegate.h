88829ea19553591508948c7e2c443181b9238cb7
damir <damir.fattahov@gmail.com>
2012-04-05 10:50:51 +0600

    Initial Commit

---
+++ Book/CZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CZAppDelegate.h
+//  Book
+//
+//  Created by Дамир Фаттахов on 05.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CZAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Book/CZAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  CZAppDelegate.m
+//  Book
+//
+//  Created by Дамир Фаттахов on 05.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CZAppDelegate.h"
+
+@implementation CZAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
